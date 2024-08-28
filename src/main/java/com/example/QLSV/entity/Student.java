fd81f72883c27556b68ed451061623419274105a
cuongcodedao <cuongdang11304@gmail.com>
2024-08-28 20:37:54 +0700

    update

--- src/main/java/com/example/QLSV/entity/Student.java
+++ src/main/java/com/example/QLSV/entity/Student.java
@@ -1,6 +1,7 @@
 package com.example.QLSV.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private int age;
     private boolean gender; // true is male and false is female
